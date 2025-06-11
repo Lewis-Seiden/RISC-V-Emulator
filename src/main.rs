cc708aed2b5b341f03919658743343438792ae05
Lewis-Seiden <lewy@seiden.us>
2025-06-11 02:11:39 -0700

    refactor example program loading

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,27 @@ mod ui;
 mod vm;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let res = run_tui_with_example();
+    let res = run_tui(vec![
+        (
+            vec![
+                0x3e, 0x80, 0x00, 0x93, 0x7d, 0x00, 0x81, 0x13, 0xc1, 0x81, 0x01, 0x93, 0x83, 0x01,
+                0x82, 0x13, 0x3e, 0x82, 0x02, 0x93, 0x00, 0x01, 0x03, 0x17, 0xfe, 0xc3, 0x03, 0x13,
+                0x00, 0x43, 0x03, 0x13, 0x00, 0x03, 0x23, 0x83,
+            ],
+            0,
+        ),
+        (vec![0xde, 0xad, 0xbe, 0xef], 0x10004),
+    ]);
     ratatui::restore();
     execute!(std::io::stdout(), DisableMouseCapture)?;
     res
 }
 
-fn run_tui_with_example() -> Result<(), Box<dyn Error>> {
+fn run_tui(to_load: Vec<(Vec<u8>, usize)>) -> Result<(), Box<dyn Error>> {
     let mut state = ArchState::new();
-    state.load(
-        vec![
-            0x3e, 0x80, 0x00, 0x93, 0x7d, 0x00, 0x81, 0x13, 0xc1, 0x81, 0x01, 0x93, 0x83, 0x01,
-            0x82, 0x13, 0x3e, 0x82, 0x02, 0x93, 0x00, 0x01, 0x03, 0x17, 0xfe, 0xc3, 0x03, 0x13,
-            0x00, 0x43, 0x03, 0x13, 0x00, 0x03, 0x23, 0x83,
-        ],
-        0,
-    );
-    state.load(vec![0xde, 0xad, 0xbe, 0xef], 0x10004);
+    for data in to_load {
+        state.load(data.0, data.1);
+    }
 
     let (mut gui, pause_rx, step_rx) = GUI::new();
 
@@ -37,7 +41,7 @@ fn run_tui_with_example() -> Result<(), Box<dyn Error>> {
 
     let arch_state_mutex = Arc::clone(&state_mutex);
     let _ = thread::spawn(move || {
-        let mut inst_count = vec![];
+        let mut inst_count = 0;
         let mut pause = true;
         while quit_rx.try_recv().is_err() {
             while pause && step_rx.try_recv().is_err() {
